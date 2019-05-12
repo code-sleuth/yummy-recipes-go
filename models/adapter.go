d9af7aeb0fcddd80da954a9ff009812cc0d9fd31
Ibrahim Mbaziira <code.ibra@gmail.com>
2019-05-13 01:49:53 +0300

    create database adapter

--- models/adapter.go
+++ models/adapter.go
@@ -4,33 +4,30 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
+	// postgres
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-var db database
-
-type database interface {
-	Create(interface{}) *gorm.DB
-	Find(out interface{}, where ...interface{}) *gorm.DB
-	Where(query interface{}, args ...interface{}) *gorm.DB
-	Save(value interface{}) *gorm.DB
-	Delete(value interface{}, where ...interface{}) *gorm.DB
-	First(out interface{}, where ...interface{}) *gorm.DB
-	AutoMigrate(value interface{}) *gorm.DB
-}
+// 	Create(interface{}) *gorm.DB
+// 	Find(out interface{}, where ...interface{}) *gorm.DB
+// 	Where(query interface{}, args ...interface{}) *gorm.DB
+// 	Save(value interface{}) *gorm.DB
+// 	Delete(value interface{}, where ...interface{}) *gorm.DB
+// 	First(out interface{}, where ...interface{}) *gorm.DB
+// 	AutoMigrate(value interface{}) *gorm.DB
 
 func dbConnect() *gorm.DB {
-	host := "localhost"
-	name := "yummy-api"
-	ssl := "disable"
 
-	db, err := gorm.Open("postgres", "host="+host+"name="+name+"ssl="+ssl)
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=code dbname=yummy-api password=inetutils sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 	return db
 }
 
-func adapter() {
+// Adapter func
+func Adapter() {
 	db := dbConnect()
-	db.AutoMigrate(User{}, Category{}, Recipe{})
+	db.AutoMigrate(&User{}, &Category{}, &Recipe{})
+	defer db.Close()
 }
